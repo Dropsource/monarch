0d848c2a3bb331f56b592f72e9f2a58a95547612
Fernando Trigoso <fertrig@gmail.com>
2020-06-02 18:31:47 -0400

    annotations documentation

--- dropsource_storybook_annotations/lib/dropsource_storybook_annotations.dart
+++ dropsource_storybook_annotations/lib/dropsource_storybook_annotations.dart
@@ -1,11 +1,4 @@
-/// The annotation `@StorybookTheme` marks a variable as a theme that Dropsource Storybook
-/// should use. The variable should be of type `ThemeData` and it should be a top-level
-/// library variable (i.e. not a local or class variable). 
-/// Example:
-/// ```
-/// @StorybookTheme('My Theme', isDefault: true)
-/// final myTheme = ThemeData(...);
-/// ```
+/// Class for the `@StorybookTheme` annotation.
 class StorybookTheme {
   
   /// The name of the theme. Storybook will use this name on the UI.
@@ -14,6 +7,15 @@ class StorybookTheme {
   /// Whether this theme is the default theme for all stories.
   final bool isDefault;
 
+  /// Marks a variable as a theme that Dropsource Storybook should use.
+  /// The variable should be of type `ThemeData` and it should be a top-level
+  /// library variable (i.e. not a local or class variable). 
+  /// 
+  /// Example:
+  /// ```
+  /// @StorybookTheme('My Theme', isDefault: true)
+  /// final myTheme = ThemeData(...);
+  /// ```
   const StorybookTheme(this.name, {this.isDefault = false}) : assert(name != null);
 }
 
