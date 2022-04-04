9501dbdedf5f051591187e9a80f9450be2662e20
Fernando Trigoso <ftrigoso@dropsource.com>
2022-04-04 16:42:56 -0400

    monarch macos app setup

---
+++ monarch_gui/monarch_platform/macos/monarch_macos/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  monarch_macos
+//
+//  Created by Fernando Trigoso on 4/4/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
