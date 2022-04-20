858c9200752e06313276e38b7d2b6617f83a1e75
Fernando Trigoso <ftrigoso@dropsource.com>
2022-04-20 13:33:48 -0400

    tools: build cli as part of main build script

--- tools/build.dart
+++ tools/build.dart
@@ -1,9 +1,11 @@
 import 'clean.dart' as clean;
+import 'build_cli.dart' as build_cli;
 import 'build_platform.dart' as build_platform;
 import 'build_controller.dart' as build_controller;
 
 void main() {
   clean.main();
+  build_cli.main();
   build_platform.main();
   build_controller.main();
 }
