a53a375ef03e22245c48b3646a03aa0805b6d50e
Fernando Trigoso <ftrigoso@dropsource.com>
2022-08-03 16:20:24 -0400

    cli: we re-reloading, start inactive heartbeat

--- cli/lib/src/task_runner/tasks_managers.dart
+++ cli/lib/src/task_runner/tasks_managers.dart
@@ -69,6 +69,9 @@ class RegenAndHotReload extends TasksManager {
     _isReloading = false;
 
     if (_needsReload) {
+      if (!heartbeat.isActive) {
+        heartbeat.start();
+      }
       reload();
     }
   }
