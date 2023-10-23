f93f187d66e6c760a05ec604e00fda3cfc29fef1
Fernando Trigoso <ftrigoso@dropsource.com>
2023-10-23 17:40:01 -0400

    tweak

--- packages/monarch/lib/src/preview/standard_themes.dart
+++ packages/monarch/lib/src/preview/standard_themes.dart
@@ -16,9 +16,9 @@ final darkTheme = MetaTheme(
 final light3Theme = MetaTheme(_light3.id, _light3.name,
     ThemeData.light(useMaterial3: true), _light3.isDefault);
 
-final dartk3Theme = MetaTheme(_dark3.id, _dark3.name,
+final dark3Theme = MetaTheme(_dark3.id, _dark3.name,
     ThemeData.dark(useMaterial3: true), _dark3.isDefault);
 
 final defaultTheme = lightTheme;
 
-final standardMetaThemes = [lightTheme, darkTheme, light3Theme, dartk3Theme];
+final standardMetaThemes = [lightTheme, darkTheme, light3Theme, dark3Theme];
